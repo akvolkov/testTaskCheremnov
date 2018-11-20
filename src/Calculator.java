a9ed1f06da4738c54e376e466899619a81fd3c63
akvolkov <aleksey_volkov88@mail.ru>
2018-11-21 01:35:16 +0300

    refactoring class

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,44 +3,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Calculator {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String expression = reader.readLine();
-        int index = 0;
-        if (expression.contains("+")) {
-            index = expression.indexOf("+");
-            int a = Integer.parseInt(expression.substring(0, index));
-            int b = Integer.parseInt(expression.substring(index + 1));
-            System.out.println("Output: " + (a+b));
-        }
-        else {
-            if (expression.contains("-")) {
-                index = expression.indexOf("-");
+    public static void main(String[] args) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            String expression = reader.readLine();
+            int index = 0;
+            if (expression.contains("+")) {
+                index = expression.indexOf("+");
                 int a = Integer.parseInt(expression.substring(0, index));
                 int b = Integer.parseInt(expression.substring(index + 1));
-                System.out.println("Output: " + (a-b));
+                System.out.println("Output: " + (a+b));
             }
             else {
-                if (expression.contains("*")){
-                    index = expression.indexOf("*");
+                if (expression.contains("-")) {
+                    index = expression.indexOf("-");
                     int a = Integer.parseInt(expression.substring(0, index));
                     int b = Integer.parseInt(expression.substring(index + 1));
-                    System.out.println("Output: " + (a*b));
+                    System.out.println("Output: " + (a-b));
                 }
                 else {
-                    if (expression.contains("/")) {
-                        index = expression.indexOf("/");
+                    if (expression.contains("*")){
+                        index = expression.indexOf("*");
                         int a = Integer.parseInt(expression.substring(0, index));
                         int b = Integer.parseInt(expression.substring(index + 1));
-                        System.out.println("Output: " + (a/b) + " ost - " + (a % b));
-
+                        System.out.println("Output: " + (a*b));
                     }
                     else {
-                        System.out.println("выражение не содержит математических знаков: + - * /");
+                        if (expression.contains("/")) {
+                            index = expression.indexOf("/");
+                            int a = Integer.parseInt(expression.substring(0, index));
+                            int b = Integer.parseInt(expression.substring(index + 1));
+                            System.out.println("Output: " + (a/b) + " ost: " + (a % b));
+
+                        }
+                        else {
+                            System.out.println("выражение не содержит математических знаков: + - * /");
+                        }
                     }
                 }
             }
         }
-        reader.close();
+        //произошло какое-либо исключение ввода-вывода
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (NumberFormatException e) {
+            System.out.println("введите строку по шаблону: [число][ариф. знак][число]");
+            e.printStackTrace();
+        }
     }
 }

a2f299cd729a98831fb88926e3ab54c71223726d
akvolkov <aleksey_volkov88@mail.ru>
2018-11-21 00:33:26 +0300

    review class Factorial

--- src/Factorial.java
+++ src/Factorial.java
@@ -5,28 +5,31 @@ import java.math.BigInteger;
 
 public class Factorial {
 
-    public static void main(String[] args) throws IOException {
-        boolean flag = true;
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        while (flag) {
-            System.out.print("input = ");
-            String stringNumber = null;
-            try {
-                stringNumber = bufferedReader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (stringNumber.equals("exit")) {
-                flag = false;
-            }
-            else {
-                BigInteger factorial = BigInteger.valueOf(1);
-                for (int i = 2; i <= Integer.parseInt(stringNumber); i++) {
-                    factorial = factorial.multiply(BigInteger.valueOf(i));
+    public static void main(String[] args) {
+        var flag = true;
+        var factorial = BigInteger.valueOf(1);
+        try(var bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (flag) {
+                System.out.print("input = ");
+                String stringNumber = bufferedReader.readLine();
+                if (stringNumber.equals("exit")) {
+                    flag = false;
                 }
-                System.out.println("factorial = " + factorial);
+                else {
+                    for (int i = 2; i <= Integer.parseInt(stringNumber); i++) {
+                        factorial = factorial.multiply(BigInteger.valueOf(i));
+                    }
+                    System.out.println("factorial = " + factorial);
+                }
+                factorial = BigInteger.valueOf(1);
             }
         }
-        bufferedReader.close();
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Input Error, " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
